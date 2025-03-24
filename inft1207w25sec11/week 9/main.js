59884c50fea2fbd220b85136ebcacdbdc0df5c70
joemathews-UX <joemathewsjacob407@gmail.com>
2025-03-23 21:53:37 -0400

    Update main.js
    Add the randomize and replaces

--- inft1207w25sec11/week 9/main.js
+++ inft1207w25sec11/week 9/main.js
@@ -12,3 +12,23 @@ const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. W
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+  
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+  
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+  
+    if(customName.value !== '') {
+      const name = customName.value;
+      newStory = newStory.replace('Bob', name);
+    }
+
+}

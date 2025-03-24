2cb96dead6b9b8308381ec42ba30118566316c54
joemathews-UX <joemathewsjacob407@gmail.com>
2025-03-23 21:59:36 -0400

    Update main.js
    add the story.Content and story.style

--- inft1207w25sec11/week 9/main.js
+++ inft1207w25sec11/week 9/main.js
@@ -7,7 +7,6 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
@@ -37,4 +36,8 @@ function result() {
         newStory = newStory.replace('94 fahrenheit', temperature);
         newStory = newStory.replace('300 pounds', weight);
       }
+      
+      story.textContent = newStory;
+      story.style.visibility = 'visible';
+
 }

c69df5273409c79b8b60f3056b79c8a4d5d602c3
joemathews-UX <joemathewsjacob407@gmail.com>
2025-04-04 21:22:16 -0400

    Update main-finished.js
    create evil circle

--- inft1207w25sec11/ASS-4/part4/main-finished.js
+++ inft1207w25sec11/ASS-4/part4/main-finished.js
@@ -171,3 +171,30 @@ while (balls.length < 25) {
 }
 
 ballCountDisplay.textContent = `Ball count: ${ballCount}`;
+
+// Create evil circle
+const evilCircle = new EvilCircle(
+  random(0, width),
+  random(0, height)
+);
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
+  }
+
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
+
+  requestAnimationFrame(loop);
+}
+
+loop();

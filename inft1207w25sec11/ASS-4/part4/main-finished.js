c60726759168572019fcc08335886d525003012d
joemathews-UX <joemathewsjacob407@gmail.com>
2025-04-04 21:06:49 -0400

    Update main-finished.js
    Evilcircle class inheriting

--- inft1207w25sec11/ASS-4/part4/main-finished.js
+++ inft1207w25sec11/ASS-4/part4/main-finished.js
@@ -38,7 +38,7 @@ class Ball extends Shape {
     this.exists = true;
   }
 
-  
+
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -82,35 +82,28 @@ class Ball extends Shape {
   }
 }
 
-const balls = [];
+// EvilCircle class inheriting from Shape
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
 
-while (balls.length < 25) {
-  const size = random(10, 20);
-  const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
-    randomRGB(),
-    size
-  );
-
-  balls.push(ball);
-}
-
-function loop() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
-  ctx.fillRect(0, 0, width, height);
-
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
   }
-
-  requestAnimationFrame(loop);
 }
-
-loop();

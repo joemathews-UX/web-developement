6e0c1d20cbf38ace81c84265df747da6b9b94b12
joemathews-UX <joemathewsjacob407@gmail.com>
2025-04-04 20:53:35 -0400

    Update main-finished.js
    add const for p

--- inft1207w25sec11/ASS-4/part4/main-finished.js
+++ inft1207w25sec11/ASS-4/part4/main-finished.js
@@ -2,6 +2,7 @@
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const ballCountDisplay = document.querySelector("p");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
